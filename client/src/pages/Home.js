4d25af9c3b92a727893a9f16ea899f8f7db85f16
민태희 <122341071+DaeGul2@users.noreply.github.com>
2024-07-16 22:25:25 +0900

    ddd
    dd

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,92 @@
-import React from 'react'
+// src/pages/Home.js
+import React from 'react';
+import { Container, Row, Col, Carousel, Button  } from 'react-bootstrap';
+import './Home.css';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+
+  const navigate = useNavigate();
+
   return (
-    <div>Home</div>
-  )
+    <Container className="home-container">
+      <Row className="mt-4">
+        <Col md={12}>
+          <video className="home-video" autoPlay muted loop>
+            <source src="/videos/promo2.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </Col>
+      </Row>
+      <Row className="mt-4">
+        <Col md={12} className="text-center">
+          <Button className="order-button" onClick={() => navigate('/menu-order')}>
+            주문하러가기
+          </Button>
+        </Col>
+      </Row>
+      <Row className="mt-4">
+        <Col md={12}>
+          <Carousel>
+            <Carousel.Item>
+              <img
+                className="d-block w-100 carousel-image"
+                src="/images/image1.jpg"
+                alt="First slide"
+              />
+              <Carousel.Caption>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100 carousel-image"
+                src="/images/image2.jpg"
+                alt="Second slide"
+              />
+              <Carousel.Caption>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100 carousel-image"
+                src="/images/image3.jpg"
+                alt="Third slide"
+              />
+              <Carousel.Caption>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100 carousel-image"
+                src="/images/image4.jpg"
+                alt="Fourth slide"
+              />
+              <Carousel.Caption>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100 carousel-image"
+                src="/images/image5.jpg"
+                alt="Fifth slide"
+              />
+              <Carousel.Caption>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100 carousel-image"
+                src="/images/image6.jpg"
+                alt="Sixth slide"
+              />
+              <Carousel.Caption>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
-export default Home
+export default Home;
